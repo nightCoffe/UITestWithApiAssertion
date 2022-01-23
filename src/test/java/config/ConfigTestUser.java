18b26ee69f85dbc4e67a3b8ae05dee4470e0196b
Your Name <youremail@domain.com>
2022-01-23 09:05:33 -0800

    added properties

--- src/test/java/config/ConfigTestUser.java
+++ src/test/java/config/ConfigTestUser.java
@@ -1,4 +1,24 @@
 package config;
 
-public class ConfigTestUser {
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "system:properties",
+        "classpath:/shopTestUser.properties"
+})
+
+public interface ConfigTestUser extends Config {
+
+    @Key("webUrl")
+    String webUrl();
+
+    @Key("apiUrl")
+    String apiUrl();
+
+    @Key("userLogin")
+    String userLogin();
+
+    @Key("userPassword")
+    String userPassword();
 }
