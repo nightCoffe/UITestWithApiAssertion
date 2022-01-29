c725c69943e2cd374cfdc5e5fbcf5a65411f41e7
Your Name <youremail@domain.com>
2022-01-29 06:40:56 -0800

    added Attach

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -4,8 +4,10 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import config.App;
 import config.UserConfig;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -17,5 +19,13 @@ public class TestBase {
         Configuration.remote = App.config.remoteUrl();
         RestAssured.baseURI = "http://demowebshop.tricentis.com";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
